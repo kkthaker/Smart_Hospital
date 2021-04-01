57fd47908275076fbf7cec2b7858c4e92a5f56c1
kkthaker <63163693+kkthaker@users.noreply.github.com>
2021-04-01 17:24:44 +0530

    initial commit

---
+++ hms/include/sidebar.php
@@ -0,0 +1,66 @@
+<div class="sidebar app-aside" id="sidebar" style="background:#66ffcc">
+				<div class="sidebar-container perfect-scrollbar" style="background:#66ffcc">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title" >
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Dashboard </span><br>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="book-appointment.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-pencil-alt"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Book Appointment </span><br>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Appointment History </span><br>
+										</div>
+									</div>
+								</a>
+							</li>
+<li>
+								<a href="manage-medhistory.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Medical History </span><br>
+										</div>
+									</div>
+								</a>
+							</li>
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
